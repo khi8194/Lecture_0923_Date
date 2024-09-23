9aed559537e7edb18cef70c67b20300e439897f8
khi8194 <khi8194@gmail.com>
2024-09-23 10:12:13 +0900

    현재 시간값을 활용해 am, pm출력 삼항연산자로 분기처리

--- js/script.js
+++ js/script.js
@@ -1,61 +1,31 @@
-/*
-new Date() : 시간관련 인스턴스 객체를 생성해주는 생성자
-시간관련 인스턴스 객체 : 다양한 시간관련 메서드들 호출 가능
-*/
+//미션1 - new Date()를 활용해서 시간, 분, 초를 각각 span 안에 출력
+//미션2 - 현재 시간이 13시를 넘기면 em 안쪽의 'am'을 'pm'으로 출력
 
-const month = [
-	"January",
-	"Feburary",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December",
-];
+// const now = new Date();
+// const hours = now.getHours();
+// const minutes = now.getMinutes();
+// const seconds = now.getSeconds();
 
-const days = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
-];
+// const hour = document.querySelectorAll("span")[0];
+// const min = document.querySelectorAll("span")[1];
+// const sec = document.querySelectorAll("span")[2];
+
+// hour.textContent = String(hours);
+// min.textContent = String(minutes);
+// sec.textContent = String(seconds);
+
+const title = document.querySelector("h1");
+// const childEl = title.children;
+// console.log(childEl);
+const [em, spanHr, spanMin, spanSec] = title.children;
+
 const now = new Date();
+const hr = now.getHours();
+const min = now.getMinutes();
+const sec = now.getSeconds();
+let apm = hr < 13 ? "am" : "pm";
 
-console.log(now);
-
-//년도값 반환
-console.log(now.getFullYear());
-
-//월 반환 (순서값을 반환)
-console.log(now.getMonth());
-console.log(month[now.getMonth()]);
-
-// 일
-console.log(now.getDate());
-
-//요일 반환 (순서값을 반환, 일:0, 월:1, 화:2, ... 토:6)
-console.log(now.getDay());
-console.log(days[now.getDay()]);
-
-//시간
-console.log(now.getHours());
-
-//분
-console.log(now.getMinutes());
-
-//초
-console.log(now.getSeconds());
-
-//우리나라 시간대를 전세계 표준시를 변환해서 반환
-const gmtNow = now.toGMTString();
-const gmtNow1 = now.toUTCString();
-console.log(gmtNow);
-console.log(gmtNow1);
+em.innerText = apm;
+spanHr.innerText = hr;
+spanMin.innerText = min;
+spanSec.innerText = sec;
