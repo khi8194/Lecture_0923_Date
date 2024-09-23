88b51305c2ce17753d8a4d57ba3e0b14772a38fe
khi8194 <khi8194@gmail.com>
2024-09-23 11:30:48 +0900

    시간 구하는 함수, 시간값을 가공해서 세팅해주는 함수로 기능 분리

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 const screen = document.querySelector(".screen");
 const [spanHr, spanMin, spanSec] = screen.children;
 
@@ -11,4 +12,30 @@ setInterval(() => {
 	spanHr.innerText = hr;
 	spanMin.innerText = min;
 	spanSec.innerText = sec;
+}, 1000);
+*/
+
+// 함수로 만들기
+const numbers = document.querySelectorAll(".screen span");
+
+//시간값을 구해서 반환하는 함수
+function getTime() {
+	const now = new Date();
+	let hr = now.getHours();
+	let min = now.getMinutes();
+	let sec = now.getSeconds();
+	return [hr, min, sec];
+}
+
+//반환 된 시간값을 DOM에 세팅하는 함수
+function setTime(num, index) {
+	//전달받은 첫번째 인수값이 한자리 숫자면 앞에 '0'붙여 출력
+	//그렇지 않으면 그냥 값 출력
+	num = num < 10 ? "0" + num : num;
+	//각 span 배열에서 두번째 인수로 전달받은 index번째 요소에 위에서 가공한 num값을 출력
+	numbers[index].innerText = num;
+}
+
+setInterval(() => {
+	getTime().forEach((num, idx) => setTime(num, idx)); // 배열을 반환 forEach연결 가능
 }, 1000);
